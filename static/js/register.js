7418ad3a9a4fcc82f07fd4e5f18456dafc6d1411
Luz Acuña <lacuna@fi.uba.ar>
2025-10-28 15:14:07 -0300

    Agrega campos de nombre y apellido + diseño y funcionalidad del boton de terminos y condiciones

--- static/js/register.js
+++ static/js/register.js
@@ -1,13 +1,26 @@
-document.getElementById("Siguiente").addEventListener("click", () => {
-  const email = document.getElementById("input_email").value.trim();
-  const confirmarEmail = document.getElementById("input_confirmar_email").value.trim();
+document.getElementById("siguiente1").addEventListener("click", () => {
+  const nombre = document.getElementById("input_nombre").value.trim();
+  const apellido = document.getElementById("input_apellido").value.trim();
 
-  if (email === "" || confirmarEmail === "") {
-    alert("Por favor completa ambos campos de email.");
+  if (nombre === "" || apellido === "") {
+    alert("Por favor completa con tu nombre y apellido");
     return;
   }
 
-  if (email !== confirmarEmail) {
+  // Mostrar paso_email y ocultar paso_nombre
+  document.getElementById("paso_nombre").style.display = "none";
+  document.getElementById("paso_email").style.display = "block";
+});
+
+document.getElementById("siguiente2").addEventListener("click", () => {
+  const email = document.getElementById("input_email").value.trim();
+  const confirmar_email = document.getElementById("input_confirmar_email").value.trim();
+
+  if (email === "" || confirmar_email === "") {
+    alert("Por favor completa ambos campos de email");
+    return;
+  }
+  else if (email !== confirmar_email) {
     alert("Los correos no coinciden. Verifícalos.");
     return;
   }
